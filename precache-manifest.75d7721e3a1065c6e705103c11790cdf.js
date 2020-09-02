bdb1d4b400bb14e67c7d7bb92f2db45830860fd6
kh45 <khaledhassan45@gmail.com>
2020-09-02 00:55:02 -0400

    Updates

---
+++ precache-manifest.75d7721e3a1065c6e705103c11790cdf.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3cb8717838fdb82e13cdb6b27832bf92",
+    "url": "/test-portfolio2/index.html"
+  },
+  {
+    "revision": "24a38efd0fe6c88b2a8b",
+    "url": "/test-portfolio2/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "3a97976c562a513a4a89",
+    "url": "/test-portfolio2/static/css/main.82b61496.chunk.css"
+  },
+  {
+    "revision": "24a38efd0fe6c88b2a8b",
+    "url": "/test-portfolio2/static/js/2.f83f49a6.chunk.js"
+  },
+  {
+    "revision": "d55b4ee296280f8c722cfe88c1e79ffa",
+    "url": "/test-portfolio2/static/js/2.f83f49a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a97976c562a513a4a89",
+    "url": "/test-portfolio2/static/js/main.bac52103.chunk.js"
+  },
+  {
+    "revision": "93257556bef63625a9e6",
+    "url": "/test-portfolio2/static/js/runtime-main.5f32b024.js"
+  },
+  {
+    "revision": "0a4e59987c51f57ad663c80c4a3459ab",
+    "url": "/test-portfolio2/static/media/AWS.0a4e5998.svg"
+  },
+  {
+    "revision": "81daa0eb1c3be074bf74d5427616dafa",
+    "url": "/test-portfolio2/static/media/SportsScraper.81daa0eb.png"
+  },
+  {
+    "revision": "48ae79d99d69739a8022593da1ece32f",
+    "url": "/test-portfolio2/static/media/angular-icon-1.48ae79d9.svg"
+  },
+  {
+    "revision": "0ff1a6f08182be4aa63a0b54da1b9e57",
+    "url": "/test-portfolio2/static/media/github.0ff1a6f0.svg"
+  },
+  {
+    "revision": "2d2e31cc63a96c7e6044f52d30fd21b9",
+    "url": "/test-portfolio2/static/media/javascript.2d2e31cc.svg"
+  },
+  {
+    "revision": "6ca50ac3027df3bae2518dbe9301ef6c",
+    "url": "/test-portfolio2/static/media/nightlife.6ca50ac3.png"
+  },
+  {
+    "revision": "b337a2fb0b1289e6757e632dc766ff6b",
+    "url": "/test-portfolio2/static/media/nodejs.b337a2fb.svg"
+  },
+  {
+    "revision": "bc1f9ae32894e04ffc833c2765303441",
+    "url": "/test-portfolio2/static/media/pokemon.bc1f9ae3.png"
+  },
+  {
+    "revision": "d7c985e7cca077dc2d77f89a66573e35",
+    "url": "/test-portfolio2/static/media/postgresql.d7c985e7.svg"
+  },
+  {
+    "revision": "eab3a1d56f5c75568c83b35b9ba49534",
+    "url": "/test-portfolio2/static/media/profpic.eab3a1d5.png"
+  },
+  {
+    "revision": "146716bdf1a351db5705ec1e6a25ed08",
+    "url": "/test-portfolio2/static/media/python.146716bd.svg"
+  },
+  {
+    "revision": "4009eef3b3e291f296e71962c70895c5",
+    "url": "/test-portfolio2/static/media/react.4009eef3.svg"
+  },
+  {
+    "revision": "aa0a337603611296524a666b62a9ff23",
+    "url": "/test-portfolio2/static/media/redux.aa0a3376.svg"
+  },
+  {
+    "revision": "8318adfc217dd334f6ceb4e567fe9dbc",
+    "url": "/test-portfolio2/static/media/ruby.8318adfc.svg"
+  },
+  {
+    "revision": "3de182d2120823fe49c27eeb1765c53a",
+    "url": "/test-portfolio2/static/media/typescript.3de182d2.svg"
+  },
+  {
+    "revision": "e0a862283b4772e4d739f3399ed7001b",
+    "url": "/test-portfolio2/static/media/uefa.e0a86228.png"
+  },
+  {
+    "revision": "8bc9131db5517713136fc24fd3970fb5",
+    "url": "/test-portfolio2/static/media/visualstudio.8bc9131d.svg"
+  }
+]);
